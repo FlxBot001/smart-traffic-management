a6f958ca4fd8c35672be57819aa5103804af11a6
CHARLES ODHIAMBO <157046505+ChuckyCharles@users.noreply.github.com>
2024-09-30 23:41:15 +0300

    Update huawei_cloud_setup.tf

--- terraform/huawei_cloud_setup.tf
+++ terraform/huawei_cloud_setup.tf
@@ -3,3 +3,14 @@ provider "huaweicloud" {
   access_key = var.access_key
   secret_key = var.secret_key
 }
+
+resource "huaweicloud_compute_instance_v2" "example" {
+  name            = "example-instance"
+  image_id        = var.image_id
+  flavor_id       = var.flavor_id
+  key_pair        = var.key_pair
+
+  network {
+    uuid = var.network_id
+  }
+}
