b8ccf9c5e146e0fa30fc241f894174d7517f15ac
CHARLES ODHIAMBO <157046505+ChuckyCharles@users.noreply.github.com>
2024-09-30 23:36:43 +0300

    Update variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  description = "The region to deploy resources"
+  type        = string
+}
+
+variable "access_key" {
+  description = "Huawei Cloud Access Key"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "Huawei Cloud Secret Key"
+  type        = string
+}
